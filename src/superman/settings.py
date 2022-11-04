c4bf95218b840caf27ed0b491d9dceb489b3d5fd
szto <sztolabs@gmail.com>
2022-11-04 17:38:39 +0900

    feat: add pants

--- src/superman/settings.py
+++ src/superman/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_pants_project.urls'
+ROOT_URLCONF = 'superman.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_pants_project.wsgi.application'
+WSGI_APPLICATION = 'superman.wsgi.application'
 
 
 # Database
